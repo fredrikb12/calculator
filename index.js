0cfa26cea299e2fe40dce3c761f72f23e668a2b8
fredrikb12 <fredrik.backman12@gmail.com>
2022-01-03 21:41:15 +0100

    Create basic calculator layout with styling, create updateDisplay(), add event listeners to number buttons.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+let currentDisplay;
 function add(num1, num2) {
     return num1 + num2;
 }
@@ -17,3 +18,16 @@ function divide(num1, num2) {
 function operate(operator, num1, num2) {
     return operator(num1, num2);
 }
+
+function updateDisplay(text) {
+    const display = document.querySelector(".display > p");
+    display.textContent = text;
+    currentDisplay = display.textContent;
+}
+
+const numberButtons = document.querySelectorAll(".number-button");
+numberButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        updateDisplay(button.textContent);
+    });
+});
